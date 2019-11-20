814332ad476e224db6a968425d0b3fa24b2b0dc7
hiiro00 <totugekida@gmail.com>
2019-11-20 11:37:30 +0000

    12xth comiit Action Cable redis nashi for Heroku8x

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   
   config.action_cable.url = "wss://#{hostname}/cable"
   # config.action_cable.allowed_request_origins = ["https://#{base_url}", "https://#{hostname}"]
-  config.action_cable.allowed_request_origins = [ /http:\/\/.*/ ]
+  config.action_cable.allowed_request_origins = [ /https:\/\/.*/ ]
   config.action_cable.disable_request_forgery_protection = true
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
